c33e001e70fb7c4f551fe7116caf69b47bb8299b
jiluo <luoji6293@163.com>
2022-05-24 19:47:27 +0800

    主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,26 @@ $.ajaxPrefilter(function(options) {
         console.log(options.url)
             //在发起真正的Ajax请求之前，统一加上请求的根路径
         options.url = 'http://www.liulongbin.top:3007' + options.url
+
+        //统一为有权限的接口设置header请求头
+        if (options.url.indexOf('/my') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+
+
+        //全局统一挂载complete回调函数
+        options.complete = function(res) {
+            // console.log(res)
+            // 在complete回调函数中，可以使用res.responseJSON拿到服务返回回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                //    强制清空token
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+
     })
     //每次调用$.get()  $.post() $.ajax()的时候，都会先调用ajaxPrefilter这个函数
     //在这个函数中可以拿到我们给Ajax的配置对象
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function() {
+    // 调用getUserInfo获取用户的基本信息
+    getUserInfo()
+
+    var layer = layui.layer
+
+    $('#btnLogout').on('click', function() {
+        // console.log('ok')
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+                // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+
+})
+
+// 获取用户的基本信息 
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+
+        success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败')
+                }
+                //   调用renderAvatar渲染用户的头像
+                renderAvatar(res.data)
+
+            }
+            // complete: function(res) {
+
+        // }
+    })
+
+
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+    //获取用户的名称
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎 &nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar')
+            .html(first)
+            .show()
+    }
+}
